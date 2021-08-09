b22c9de055349adeac0ad6f4288cd023083e8dbf
MUZAMMIL NAWAZ <muzammilnawaz@MUZAMMILs-MacBook-Air.local>
2021-08-09 11:23:53 +0530

    added timestamped logs

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 from src.app import App
 
 # configuring the logger file
-logging.basicConfig(filename='./logs/logs.conf', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='./logs/logs.conf', encoding='utf-8', level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',)
 
 log = logging.getLogger(__name__)
 

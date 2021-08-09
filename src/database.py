fc9eeead3c26cecbf7f518a70ac2640c7876d235
MUZAMMIL NAWAZ <muzammilnawaz@MUZAMMILs-MacBook-Air.local>
2021-08-09 10:54:01 +0530

    commented app

--- src/database.py
+++ src/database.py
@@ -5,11 +5,14 @@ log = logging.getLogger(__name__)
 
 
 class Database:
+
+    # configures connection uri
     def __init__(self):
         self.uri = f"dbname={config['db_name']} user={config['db_user']} password={config['db_password']}"
         self.conn = None
         self.cur = None
 
+    # connect to database using uri and sets cursor
     def connect(self):
         try:
             self.conn = psycopg2.connect(self.uri)
@@ -20,6 +23,8 @@ class Database:
             log.err(err)
             raise Exception("Failed to connect to Database")
 
+    # commits the sql queries executed during execution
+    # and closes the cursor and connection
     def close(self):
         self.conn.commit()
         self.cur.close()

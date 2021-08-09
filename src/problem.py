f9fa9acc125f0502e77b2010728abefd0d0570ee
MUZAMMIL NAWAZ <muzammilnawaz@MUZAMMILs-MacBook-Air.local>
2021-08-09 11:09:45 +0530

    commented problem

--- src/problem.py
+++ src/problem.py
@@ -1,3 +1,9 @@
+
+# base class  for
+# all problem class inherits this class
+# initially the result is false
+# after successfully solving the problem
+# child class will set result value to True
 class Problem:
     def __init__(self, cur):
         self.cur = cur

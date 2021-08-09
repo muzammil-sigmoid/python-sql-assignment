75454d0f75558fd271c6cd5d816b3c920688b232
MUZAMMIL NAWAZ <muzammilnawaz@MUZAMMILs-MacBook-Air.local>
2021-08-09 11:20:11 +0530

    commented config file

--- res/config.py
+++ res/config.py
@@ -1,8 +1,8 @@
 from res.secret import db_password
 config = {
-    "db_password" : db_password,
-    "db_user" : "postgres",
-    "db_name": "assignment",
-    "out_folder_path": "./out_data/"
+    "db_password" : db_password, # replace it with your own password
+    "db_user" : "postgres",   # replace it with your own username
+    "db_name": "assignment", # replace it with your own db_name
+    "out_folder_path": "./out_data/"   # replace it with your own data folder
 
 }
